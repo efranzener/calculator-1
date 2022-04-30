6c871d9cb9fe5ed7cdcd630c7757edb749475312
efranzener <efranzener.mp@gmail.com>
2022-04-30 16:57:27 -0700

    Completed cube function

--- arithmetic.py
+++ arithmetic.py
@@ -18,13 +18,14 @@ def multiply(num1, num2):
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
 
-    return num1 / num2
+    return num1 / num2ctl
 def square(num1):
     """Return the square of num1."""
     return num1 * num1
 
 def cube(num1):
     """Return the cube of num1."""
+    return num1 * num1 * num1
     
 
 def power(num1, num2):
